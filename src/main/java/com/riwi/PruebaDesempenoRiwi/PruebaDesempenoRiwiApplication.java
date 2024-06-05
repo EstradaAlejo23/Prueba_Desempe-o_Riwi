044dde84a1c9b83d4810625af28583e84fbf8c83
Alejandro Estrada <estradaprograma.23.01@gmail.com>
2024-06-05 06:29:08 -0500

    creando proyecto

---
+++ src/main/java/com/riwi/PruebaDesempenoRiwi/PruebaDesempenoRiwiApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.PruebaDesempenoRiwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDesempenoRiwiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDesempenoRiwiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/PruebaDesempenoRiwi/PruebaDesempenoRiwiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.PruebaDesempenoRiwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaDesempenoRiwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
